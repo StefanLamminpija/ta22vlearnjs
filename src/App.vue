8e3a42a82780b5de2d212220524582f8f312e937
Stefan Lamminpija <stefan.lamminpija@tptlive.ee>
2023-12-14 10:22:56 +0200

    app changes

--- src/App.vue
+++ src/App.vue
@@ -2,43 +2,45 @@
     import { ref, reactive } from 'vue';
 
     let message = ref('Hello Vue');
-    let count = ref(0);
     let newItem = ref('');
+    let count = ref(0);
     let items = reactive([
         'Leib',
         'Piim',
-        'Viin',
+        'Viin'
     ]);
     function addItem(){
-        if(newItem.value.trim() !== '') {
-        items.push(newItem.value.trim());
+        if(newItem.value.trim() !== ''){
+            items.push(newItem.value.trim());
         }
         newItem.value = '';
     }
-
 </script>
 
 <template>
 
     <div class="container">
 
-        {{ count }}
+        {{ count }} 
         <button class="button is-primary" @click="count++">Count: {{ count }}</button>
 
-        <!-- <input type="text" :value="message" class="input mt-2" @input="message=$event.target.value">-->
+        <!-- <input type="text" :value="message" class="input mt-2" @input="message=$event.target.value"> -->
 
         <input type="text" v-model="message" class="input mt-2">
         {{ message.split('').reverse().join('') }}
 
         <input type="text" v-model="newItem" class="input mt-2" @keydown.enter="addItem">
-        <button class="button is-primary" @click="addItem"> Add Item </button>
+
+        <button class="button is-primary" @click="addItem">Add Item</button>
 
         <div class="content">
 
-            <li v-for="item in items"> {{ item }} </li>
+            <ul>
+                <li v-for="item in items">{{ item }}</li>
+            </ul>
+            
+        </div> <!-- content -->
 
-        </div>
-
-    </div>
+    </div> <!-- container -->
 
 </template>
